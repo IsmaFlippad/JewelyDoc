dfa2be34e6ed42e3bb8c943fa7dbb9a8a5853438
Isma Flippad <92794999+IsmaFlippad@users.noreply.github.com>
2025-03-20 17:51:09 +0100

    fixing CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Ajoutez d'autres configurations si nécessaire
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/app/api/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
