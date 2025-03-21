e2efcccd4a6353887844ff83163ded13ec4e9a6f
Isma Flippad <92794999+IsmaFlippad@users.noreply.github.com>
2025-03-21 13:10:28 +0100

    Build the next project in vercel install process

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   // Set the production url of your site here
   url: "https://v0-docusaurus-setup.vercel.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: "/",
+  baseUrl: "/frontend/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
