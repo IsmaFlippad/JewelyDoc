67f4c6fd02172f99b901daf2568e68e2463eb3bf
Isma Flippad <92794999+IsmaFlippad@users.noreply.github.com>
2025-03-21 15:14:25 +0100

    🚚 Réorganisation des fichiers pour intégrer Next.js

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   rewrites: async () => [
     {
       source: "/:path*", // Toutes les routes
-      destination: "/frontend/build/:path*", // Fichiers statiques de Docusaurus
+      destination: "/frontend/:path*", // Fichiers statiques de Docusaurus
     },
   ],
 };
